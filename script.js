31a43e7214f741a65cb18e08c3eff843342bad84
Baboon1098 <137217755+Baboon1098@users.noreply.github.com>
2023-07-19 12:02:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function generateRandomPassword(length) {
+  var upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+  var numbers = '0123456789';
+  var allCharacters = upperCaseLetters + lowerCaseLetters + numbers;
 
-  passwordText.value = password;
+  let password = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * allCharacters.length);
+    password += allCharacters.charAt(randomIndex);
+  }
 
+  return password;
+}
+
+function generatePassword() {
+  var passwordLength = 12;
+  var randomPassword = generateRandomPassword(passwordLength);
+  document.getElementById('password').textContent = randomPassword;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
