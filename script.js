4925791a3e108560115159915cdeca063dff525b
Baboon1098 <137217755+Baboon1098@users.noreply.github.com>
2023-07-19 12:31:35 -0500

    Final
    This is the end code and also adds the directions to the file

--- script.js
+++ script.js
@@ -5,7 +5,44 @@ function generateRandomPassword(length) {
   var upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   var lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz';
   var numbers = '0123456789';
-  var allCharacters = upperCaseLetters + lowerCaseLetters + numbers;
+  var Special = "!@#$%^&*-+=";
+
+  var allCharacters = "";
+  if (length < 8){
+    alert("you must add at least 8 characters for the password to be added");
+    return "";
+  }
+
+  if(length > 128){
+    alert("The password can only have 128 characters");
+    return "";
+  }
+
+  var Lower = confirm("Do you want to add Lower Case Letters?");
+  var uper = confirm("Do you want to add Upper Case Letters?");
+  var number = confirm("Do you want to add Numbers?");
+  var Specials = confirm("Do you want to add Specials");
+
+  if (Lower == true){
+    allCharacters = allCharacters + lowerCaseLetters;
+  }
+
+  if (uper == true){
+    allCharacters = allCharacters + upperCaseLetters;
+  }
+
+  if (number == true){
+    allCharacters = allCharacters + numbers;
+  }
+
+  if (Specials == true){
+    allCharacters = allCharacters + Special;
+  }
+  
+  if(allCharacters == ""){
+    alert("You need at least one set of Characters for the Password to work");
+    return "";
+  }
 
   let password = '';
   for (let i = 0; i < length; i++) {
@@ -17,7 +54,7 @@ function generateRandomPassword(length) {
 }
 
 function generatePassword() {
-  var passwordLength = 12;
+  var passwordLength = prompt("Enter how long you want the Password to be");
   var randomPassword = generateRandomPassword(passwordLength);
   document.getElementById('password').textContent = randomPassword;
 }
